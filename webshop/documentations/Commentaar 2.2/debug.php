5d82b25c3ebdbfd37b108016812e6dee87f24ded
Haenkos <Haenkos@protonmail.com>
2022-01-17 16:49:12 +0100

    delete documentations directory

--- webshop/documentations/Commentaar 2.2/debug.php
+++
@@ -1,8 +0,0 @@
-<?php
-function console_log($data) { /* JH: Hmm dit is javascript, was niet onderdeel van de opdracht */
-    echo '<script>';
-    echo 'console.log('. json_encode($data) .')';
-    echo '</script>';
-}
-
-?>
